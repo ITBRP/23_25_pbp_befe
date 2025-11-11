64786aba4612a20ab72a1344932f246820a359e2
Aldhi <aldhi.albadri@gmail.com>
2025-11-11 10:30:31 +0700

    fix materi febe

--- be/create.php
+++ be/create.php
@@ -1,80 +1,90 @@
 <?php
+header("Content-Type: application/json; charset=UTF-8");
 // validasi cek method
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
-    echo json_encode(['status' => 'error', 'message' => 'Method Salah !']);
+    echo json_encode([
+        'status' => 'error', 
+        'msg' => 'Method Salah !'
+    ]);
     exit;
 }
 
-//Mempersiapkan datanya dan variabel $data 
-header("Content-Type: application/json; charset=UTF-8");
-$input = file_get_contents("php://input");
-$data = json_decode($input, true);
-
-// validasi error
 $errors = [];
-if (!isset($data['nim'])) {
-    $errors['nim'] = "Data NIM tidak ada";
-} else {
-    if ($data['nim'] == '') {
-        $errors['nim'] = "Data NIM tidak boleh kosong";
+if(!isset($_POST['nama'])){
+    $errors['nama'] = "Nama tidak di kirim";
+}else{
+    if($_POST['nama']==""){
+        $errors['nama'] = "Nama tidak boleh kosong";
     }
 }
 
-if (!isset($data['nama'])) {
-    $errors['nama'] = "Data NAMA tidak ada";
-} else {
-    if ($data['nama'] == '') {
-        $errors['nama'] = "Data NAMA tidak boleh kosong";
+if(!isset($_POST['nim'])){
+    $errors['nim'] = "nim tidak di kirim";
+}else{
+    if($_POST['nim']==""){
+        $errors['nim'] = "nim tidak boleh kosong";
     }
 }
 
-if (!isset($data['hp'])) {
-    $errors['hp'] = "Data HP tidak ada";
-} else {
-    if ($data['hp'] == '') {
-        $errors['hp'] = "Data HP tidak boleh kosong";
-    }
-}
-
-if (!isset($data['alamat'])) {
-    $errors['alamat'] = "Data ALAMAT tidak ada";
-} else {
-    if ($data['alamat'] == '') {
-        $errors['alamat'] = "Data alamat tidak boleh kosong";
-    }
-}
-
-if (count($errors) == 0) {
-    // koneksi
-    // $koneksi = mysqli_connect('localhost', 'root','', 'PBP_PAGIA');
-    $koneksi = new mysqli('localhost', 'root','', 'PBP_PAGIA');
-    // insert
-    $nim = $data['nim'];
-    $nama = $data['nama'];
-    $hp = $data['hp'];
-    $alamat = $data['alamat'];
-    $q = "INSERT INTO mahasiswa (nim, nama, hp, alamat) VALUES('$nim','$nama','$hp','$alamat')";
-    // mysqli_query($koneksi, $q);
-    $koneksi->query($q);
-    $dataResponse = [
-        'status' => 'success',
-        'msg' => 'Data baru berhasil dibuat',
-        'data' => [
-            'id' => $koneksi->insert_id,
-            'nim' => $data['nim'],
-            'nama' => $data['nama'],
-            'hp' => $data['hp'],
-            'alamat' => $data['alamat'],
-        ]
-    ];
-} else {
-    $dataResponse = [
-        'status' => 'error',
-        'msg' => 'Validasi Error',
-        'errors' => $errors
-    ];
+if(count($errors)>0){
     http_response_code(400);
+    echo json_encode([
+        'status' => 'error', 
+        'msg' => 'Data tidak valid !',
+        'errors' => $errors
+    ]);
+    exit;
 }
 
-echo json_encode($dataResponse);
+echo json_encode([
+    'status' => 'success', 
+    'errors' => $errors
+]);
+
+// $errors = [];
+// if (!isset($_POST['nama'])) {
+//     $errors['nama'] = "Data Nama tidak ada";
+// } else {
+//     if ($_POST['nama'] == '') {
+//         $errors['nama'] = "Data Nama tidak boleh kosong";
+//     }
+// }
+// if (!isset($_POST['nim'])) {
+//     $errors['nim'] = "Data NIM tidak ada";
+// } else {
+//     if ($_POST['nim'] == '') {
+//         $errors['nim'] = "Data NIM tidak boleh kosong";
+//     }
+// }
+
+// if (count($errors) > 0) {
+//     $dataResponse = [
+//         'status' => 'error',
+//         'msg' => 'Validasi Error',
+//         'errors' => $errors
+//     ];
+//     http_response_code(400);
+// }
+
+//     // koneksi
+//     $koneksi = new mysqli('localhost', 'root','', 'PBP_PAGIA');
+//     // insert
+//     $nim = $data['nim'];
+//     $nama = $data['nama'];
+//     $q = "INSERT INTO mahasiswa (nim, nama) VALUES('$nim','$nama')";
+//     // mysqli_query($koneksi, $q);
+//     $koneksi->query($q);
+//     $dataResponse = [
+//         'status' => 'success',
+//         'msg' => 'Data baru berhasil dibuat',
+//         'data' => [
+//             'id' => $koneksi->insert_id,
+//             'nim' => $data['nim'],
+//             'nama' => $data['nama'],
+//         ]
+//     ];
+
+
+// echo json_encode($dataResponse);
+
